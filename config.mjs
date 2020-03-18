3924ccf07c09933197a93c60ae27290fd2e2d48d
cosmopool <dash.zet@gmail.com>
2020-03-18 11:51:51 -0300

    not working yet

---
+++ config.mjs
@@ -0,0 +1,13 @@
+export default
+    {
+        leftX: 0,
+        leftY: 0,
+        rightX: 0,
+        rightY: 0,
+        lt: 0,
+        rt: 0,
+        a: 0,
+        b: 0,
+        x: 0,
+        y: 0
+    }
