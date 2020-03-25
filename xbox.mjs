f2621637a8951678c3a5bc74468115933e6cac34
cosmopool <dash.zet@gmail.com>
2020-03-25 16:00:10 -0300

    last fixes

--- xbox.mjs
+++ xbox.mjs
@@ -1,54 +1,111 @@
 import * as child_process from 'child_process'
-import coord from './config.mjs'
+import coordinates from './config.mjs'
 
 const spawn = child_process.spawn
-const xboxdrv = spawn('xboxdrv', ['--no-uinput', '--detach-kernel-driver'])
 
-let res = []
+export default function xbox() {
 
-function xbox(data) {
-	//console.log(`data: ${data.toString()}, ${data}`)
+	// function to pass coordinates on arr to json object
+	function toCoord(newCoord, originalCoord) {
+		//coordinate index
+		let index = resultArr.indexOf(originalCoord)
 
-	// transform the data string to array
-	//data.toString().data.split(":").join(" ").split(" ").forEach(x => { if (x != "") { res.push(x) } })
-	data.toString()
+		//value is the next element of coordinate index
+		let value = Number(resultArr[index + 1])
 
+		//deadzone implementation
+		if (newCoord === 'leftX' || newCoord === 'leftY' || newCoord === 'rightX' || newCoord === 'rightY') {
+			//modifies the axis range
+			//console.log(`coord: ${newCoord}, value: ${value}, newValue: ${newRange(value)}`)
+			value = newRange(value)
 
-	// function to pass the joystick coordinates to json object
-	function toCoord(x, n) {
-		coord[x] = Number(res[n + 1])
+			if (value < deadzone && value > -deadzone) {
+				value = 0
+			}
+		}
+
+		//pass coordinate and value to object
+		coordinates[newCoord] = value
 	}
 
-	// loop every element of result array and call toCoord function
-	res.forEach(x => {
-		switch (x) {
-			case 'X1': toCoord("leftX", res.indexOf(x)); break;
-			case 'Y1': toCoord("leftY", res.indexOf(x)); break;
-			case 'X2': toCoord("rightX", res.indexOf(x)); break;
-			case 'Y2': toCoord("rightY", res.indexOf(x)); break;
-			case 'lt': toCoord("lt", res.indexOf(x)); break;
-			case 'rt': toCoord("rt", res.indexOf(x)); break;
-			case 'a': toCoord("a", res.indexOf(x)); break;
-			case 'b': toCoord("b", res.indexOf(x)); break;
-			case 'x': toCoord("x", res.indexOf(x)); break;
-			case 'y': toCoord("y", res.indexOf(x)); break;
+	function newRange (oldValue) {
+		let oldMax = 32767
+		let oldMin = -32768
+		let newMax = 255
+		let newMin = -255
+		let oldRange = (oldMax - oldMin)
+		let newRange = (newMax - newMin)
+		let newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin
+
+		return Math.round(newValue)
+	}
+
+	const xboxdrv = spawn('xboxdrv', ['--no-uinput', '--detach-kernel-driver'])
+	const deadzone = 50
+
+	let resultArr = []
+	let prevCoordinates = {}
+	let jsonCoord
+
+	function dataToCoordinates(data){
+
+		//clean the result array to not accumulate old values
+		resultArr = []
+
+		//data to string
+		let dataString = data.toString()
+
+		// transform data to array
+		dataString.split(":").join(" ").split(" ").forEach(x => { if (x != "") { resultArr.push(x) } })
+
+		// pass old and new coordinates to toCoord function
+		resultArr.forEach(x => {
+			switch (x) {
+				case 'X1': toCoord('leftX', x); break;
+				case 'Y1': toCoord('leftY', x); break;
+				case 'X2': toCoord('rightX', x); break;
+				case 'Y2': toCoord('rightY', x); break;
+				case 'LT': toCoord('leftT', x); break;
+				case 'RT': toCoord('rightT', x); break;
+				case 'A': toCoord('a', x); break;
+				case 'B': toCoord('b', x); break;
+				case 'X': toCoord('x', x); break;
+				case 'Y': toCoord('y', x); break;
+					//we do not need deafult case
+				default: ; break;
+			}
+		})
+
+		if (JSON.stringify(coordinates) === prevCoordinates){
+			return 0
+		} else {
+			prevCoordinates = JSON.stringify(coordinates)
+			jsonCoord = JSON.stringify(coordinates)
+			//console.log(`${JSON.stringify(coordinates)}`)
+			//return JSON.stringify(coordinates)
 		}
+	}
+
+	xboxdrv.stdout.on('data', dataToCoordinates.bind(this))
+
+	xboxdrv.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`)
 	})
-	//return res
-	return console.log(data.toString)
+
+	xboxdrv.on('close', (code) => {
+		console.log(`child process exited with code ${code}`)
+	})
 }
 
-xboxdrv.stdout.on('data', (data) => {
-	//console.log(`out: ${xbox(data)}`)
-	console.log(`out: ${data}`)
-})
+xbox.prototype.all = () => {
+	return JSON.stringify(coordinates)
+}
 
-xboxdrv.stderr.on('data', (data) => {
-	console.log(`stderr: ${data}`);
-});
 
-console.log(`coord: ${coord}`)
-
-xboxdrv.on('close', (code) => {
-	console.log(`child process exited with code ${code}`);
-});
+xbox.prototype.print = () => {
+	console.log(`test PRINTTTTTT`)
+	//console.log(jsonCoord)
+	//return console.log(this.jsonCoord)
+	//xbox.prototype.call()
+}
+//xbox().print()
